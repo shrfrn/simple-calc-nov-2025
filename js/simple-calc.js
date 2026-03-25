ff8afed09aba0fddcfb1b79a6e68db57409d07f8
shrfrn <shrfrn@gmail.com>
2026-03-25 20:19:56 +0200

    Implement onClickNum and onOperatorClick

--- js/simple-calc.js
+++ js/simple-calc.js
@@ -1,11 +1,17 @@
 'use strict'
 
+var num1 = 0
+var num2 = 0
+var operator = ''
+var x = 10
+
 function onNumClick(elCell) {
-    console.log('Clicked', elCell.innerText)
+    if(!num1) num1 = +elCell.innerText
+    else if(operator && !num2) num2 = +elCell.innerText
 }
 
 function onOperatorClick(elCell) {
-    console.log('Clicked', elCell.innerText)
+    if(num1 && !num2) operator = elCell.innerText
 }
 
 function onCalc() {
