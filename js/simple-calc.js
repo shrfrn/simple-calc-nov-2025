6dbf3351d9d1c62d2814aa34e9fff97333152db4
shrfrn <shrfrn@gmail.com>
2026-03-25 19:54:29 +0200

    Add click handler placeholders

---
+++ js/simple-calc.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function onNumClick(elCell) {
+    console.log('Clicked', elCell.innerText)
+}
+
+function onOperatorClick(elCell) {
+    console.log('Clicked', elCell.innerText)
+}
+
+function onCalc() {
+    console.log('Caculating')
+}
