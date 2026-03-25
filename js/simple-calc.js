0059c829eaedeab2350196c7eb214d1a9b13e712
shrfrn <shrfrn@gmail.com>
2026-03-25 21:07:13 +0200

    Add print out on click

--- js/simple-calc.js
+++ js/simple-calc.js
@@ -8,16 +8,18 @@ var x = 10
 function onNumClick(elCell) {
     if(!num1) num1 = +elCell.innerText
     else if(operator && !num2) num2 = +elCell.innerText
+    console.log(`${num1} ${operator} ${num2}`)
 }
 
 function onOperatorClick(elCell) {
     if(num1 && !num2) operator = elCell.innerText
+    console.log(`${num1} ${operator} ${num2}`)
 }
 
 function onCalc() {
     if(num1 && num2 && operator){
         console.log(`${num1} ${operator} ${num2} = ${+num1 + +num2}`)
-        num1 = num2 = 0
+        num1 = num2 = ''
         operator = ''
     }
 }
